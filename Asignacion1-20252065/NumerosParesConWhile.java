193599a95f7c4c05248bed1e4ed5cd928da04639
Luisrivera1211 <129557001+Luisrivera1211@users.noreply.github.com>
2026-02-05 20:57:21 -0400

    Add files via upload

---
+++ Asignacion1-20252065/NumerosParesConWhile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numerosparesconwhile;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NumerosParesConWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 2;
+                
+        while(num <= 100){
+            System.out.println(num);
+            num += 2;
+        }  
+    }
+    
+}
