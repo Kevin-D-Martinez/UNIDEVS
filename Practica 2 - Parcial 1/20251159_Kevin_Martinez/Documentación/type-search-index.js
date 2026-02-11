6faca8e149a0a607627128eee3920b98b127c6da
Kevin Martinez <kevindanielmartinezreyes@gmail.com>
2026-02-11 00:30:22 -0400

    Reorganización de carpetas

---
+++ Practica 2 - Parcial 1/20251159_Kevin_Martinez/Documentación/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CalculadoraMatematica"},{"p":"<Unnamed>","l":"Matematica"}];updateSearchResults();
